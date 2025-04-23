0b92fe2e5f9c32260fac125c9336d7c240375d7e
KaioMenezesB <kaio_borduque@hotmail.com>
2025-04-23 18:47:53 -0300

    Atividade finalizada

--- detector-borda.v
+++ detector-borda.v
@@ -4,7 +4,21 @@ module detector_borda (
     input [1:0] entrada,
     output [1:0] detector
 );
-    
-//insira seu código aqui
+
+    reg [1:0] prev;
+    reg [1:0] detector_reg;
+
+    assign detector = detector_reg;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            prev         <= 2'b00;
+            detector_reg <= 2'b00;
+            
+        end else begin
+            detector_reg <= entrada & ~prev;
+            prev         <= entrada;
+        end
+    end
 
 endmodule
